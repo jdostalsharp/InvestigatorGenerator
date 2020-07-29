f30eb4f3b813e7e3825fc9c7b19ef321440ff74f
jdostalsharp <s4317001@student.uq.edu.au>
2020-07-29 20:43:30 +1000

    Added some more comments to Investigator Testing the secondary branch

--- src/Character/Investigator.java
+++ src/Character/Investigator.java
@@ -24,18 +24,34 @@ public class Investigator {
         this.age = age;
     }
 
+    /**
+     * Get the name of the character
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the investigator
+     * @param name != null
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the age of the investigator
+     * @return age
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Sets the age of the investigator
+     * @param age > 11  && <= 100
+     */
     public void setAge(int age) {
         this.age = age;
     }
